27a6a3ec96549912c92393016d8413ff207318e5
Justin Harrison <justin.t.harrison@gmail.com>
2015-08-29 23:29:48 -0500

    All

---
+++ PalindromesV2/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace PalindromesV2
+{
+    partial class Palindromes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CheckButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.palindromeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CheckButton
+            // 
+            this.CheckButton.AutoSize = true;
+            this.CheckButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckButton.Location = new System.Drawing.Point(16, 108);
+            this.CheckButton.Name = "CheckButton";
+            this.CheckButton.Size = new System.Drawing.Size(74, 34);
+            this.CheckButton.TabIndex = 0;
+            this.CheckButton.TabStop = false;
+            this.CheckButton.Text = "Check";
+            this.CheckButton.UseVisualStyleBackColor = true;
+            this.CheckButton.Click += new System.EventHandler(this.CheckButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Is it a palindrome?";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(142, 116);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(74, 26);
+            this.resultLabel.TabIndex = 2;
+            this.resultLabel.Text = "Result";
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(229, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.palindromeToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // palindromeToolStripMenuItem
+            // 
+            this.palindromeToolStripMenuItem.Name = "palindromeToolStripMenuItem";
+            this.palindromeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.palindromeToolStripMenuItem.Text = "Palindrome?";
+            this.palindromeToolStripMenuItem.Click += new System.EventHandler(this.palindromeToolStripMenuItem_Click);
+            // 
+            // Palindromes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 162);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CheckButton);
+            this.Controls.Add(this.menuStrip1);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Palindromes";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Palindromes";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CheckButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem palindromeToolStripMenuItem;
+    }
+}
+
---
+++ PalindromesV2/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace PalindromesV2
+{
+    public partial class Palindromes : Form
+    {
+        public Palindromes()
+        {
+            InitializeComponent();
+            textBox1.Focus();
+        }
+
+        private void CheckButton_Click(object sender, EventArgs e)
+        {
+            bool result = IsA_Palindrome(textBox1.Text);
+            resultLabel.Text = result.ToString();
+            textBox1.Focus();
+        }
+
+        private bool IsA_Palindrome(string palin)
+        {
+            string s1 = palin.ToLower(); //taking input and setting to lowercase
+
+            //This will remove everything in the string that IS NOT in the regex arguement "[^a-z]". Only letters remain. 
+            string forwardString = Regex.Replace(s1, "[^a-z]", "", RegexOptions.Compiled);
+
+            //Handling the reversal of the string
+            char[] zReversed = forwardString.ToCharArray();  //Turing string into an array of characters
+            Array.Reverse(zReversed);  //Reversing that array
+            string backwardString = new string(zReversed);  //Reforming the new string
+
+            return forwardString.Equals(backwardString);  //Will return true if the strings match.
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                CheckButton_Click(sender, e);
+            }
+        }
+
+        private void palindromeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A palindrome is a word, phrase, number, or other sequence of characters which " +
+                            "reads the same backward or forward. Allowances may be made for adjustments to " +
+                            "capital letters, punctuation, and word dividers.", "Palindromes?");
+        }
+    }
+}
